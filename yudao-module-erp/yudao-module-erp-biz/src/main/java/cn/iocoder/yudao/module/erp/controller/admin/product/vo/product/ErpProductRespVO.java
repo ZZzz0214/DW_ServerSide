058e8150aee1c9790cf69ac49652aa61d6174150
xieshuaibi <237514594@qq.com>
2025-03-25 21:42:46 +0800

    后端的产品saveVo修改完成  基本流程增删改查没有问题

--- yudao-module-erp/yudao-module-erp-biz/src/main/java/cn/iocoder/yudao/module/erp/controller/admin/product/vo/product/ErpProductRespVO.java
+++ yudao-module-erp/yudao-module-erp-biz/src/main/java/cn/iocoder/yudao/module/erp/controller/admin/product/vo/product/ErpProductRespVO.java
@@ -13,17 +13,6 @@ import java.time.LocalDateTime;
 @ExcelIgnoreUnannotated
 public class ErpProductRespVO {
 
-    /**添加**/
-    @Schema(description = "单位编号", requiredMode = Schema.RequiredMode.REQUIRED, example = "8869")
-    private Long unitId;
-    @Schema(description = "单位", requiredMode = Schema.RequiredMode.REQUIRED, example = "个")
-    @ExcelProperty("单位")
-    private String unitName;
-    @Schema(description = "产品分类", requiredMode = Schema.RequiredMode.REQUIRED, example = "水果")
-    @ExcelProperty("产品分类")
-    private String categoryName;
-
-
     @Schema(description = "产品编号", requiredMode = Schema.RequiredMode.REQUIRED, example = "15672")
     @ExcelProperty("产品编号")
     private Long id;
@@ -31,10 +20,6 @@ public class ErpProductRespVO {
     @Schema(description = "产品名称", requiredMode = Schema.RequiredMode.REQUIRED, example = "李四")
     @ExcelProperty("产品名称")
     private String name;
-
-    @Schema(description = "产品条码", requiredMode = Schema.RequiredMode.REQUIRED, example = "X110")
-    @ExcelProperty("产品条码")
-    private String barCode;
 
     @Schema(description = "产品图片")
     @ExcelProperty("产品图片")
@@ -48,8 +33,54 @@ public class ErpProductRespVO {
     @ExcelProperty("发货编码")
     private String shippingCode;
 
-    @Schema(description = "商品备案")
-    @ExcelProperty("商品备案")
+    @Schema(description = "单位编号", requiredMode = Schema.RequiredMode.REQUIRED, example = "8869")
+    private Long unitId;
+
+    @Schema(description = "单位", requiredMode = Schema.RequiredMode.REQUIRED, example = "个")
+    @ExcelProperty("单位")
+    private String unitName;
+
+    @Schema(description = "产品规格")
+    @ExcelProperty("产品规格")
+    private String standard;
+
+    @Schema(description = "产品重量（单位：kg）")
+    @ExcelProperty("产品重量（单位：kg）")
+    private BigDecimal weight;
+
+    @Schema(description = "产品日期")
+    @ExcelProperty("产品日期")
+    private LocalDateTime productionDate;
+
+    @Schema(description = "保质期天数")
+    @ExcelProperty("保质期天数")
+    private Integer expiryDay;
+
+    @Schema(description = "品牌名称")
+    @ExcelProperty("品牌名称")
+    private String brand;
+
+    @Schema(description = "产品品类编号", requiredMode = Schema.RequiredMode.REQUIRED, example = "11161")
+    private Long categoryId;
+
+    @Schema(description = "产品分类", requiredMode = Schema.RequiredMode.REQUIRED, example = "水果")
+    @ExcelProperty("产品分类")
+    private String categoryName;
+
+    @Schema(description = "产品状态", requiredMode = Schema.RequiredMode.REQUIRED, example = "2")
+    @ExcelProperty("产品状态")
+    private Integer status;
+
+    @Schema(description = "产品卖点")
+    @ExcelProperty("产品卖点")
+    private String productSellingPoints;
+
+    @Schema(description = "条形编号")
+    @ExcelProperty("条形编号")
+    private String barCode;
+
+    @Schema(description = "备案编号")
+    @ExcelProperty("备案编号")
     private String productRecord;
 
     @Schema(description = "执行编号")
@@ -59,49 +90,6 @@ public class ErpProductRespVO {
     @Schema(description = "商标编号")
     @ExcelProperty("商标编号")
     private String trademarkCode;
-
-    @Schema(description = "产品分类编号", requiredMode = Schema.RequiredMode.REQUIRED, example = "11161")
-    private Long categoryId;
-
-    @Schema(description = "品牌")
-    @ExcelProperty("品牌")
-    private String brand;
-
-    @Schema(description = "产品状态", requiredMode = Schema.RequiredMode.REQUIRED, example = "2")
-    @ExcelProperty("产品状态")
-    private Integer status;
-
-    @Schema(description = "产品备注")
-    @ExcelProperty("产品备注")
-    private String remark;
-
-    @Schema(description = "产品卖点")
-    @ExcelProperty("产品卖点")
-    private String productSellingPoints;
-
-    @Schema(description = "产品规格")
-    @ExcelProperty("产品规格")
-    private String standard;
-
-    @Schema(description = "产品重量（kg）")
-    @ExcelProperty("产品重量（kg）")
-    private BigDecimal weight;
-
-    @Schema(description = "产品长宽高")
-    @ExcelProperty("产品长宽高")
-    private String productDimensions;
-
-    @Schema(description = "箱规")
-    @ExcelProperty("箱规")
-    private String cartonSpecifications;
-
-    @Schema(description = "箱规长宽高")
-    @ExcelProperty("箱规长宽高")
-    private String cartonDimensions;
-
-    @Schema(description = "箱规重量")
-    @ExcelProperty("箱规重量")
-    private Double cartonWeight;
 
     @Schema(description = "现货数量")
     @ExcelProperty("现货数量")
@@ -115,6 +103,22 @@ public class ErpProductRespVO {
     @ExcelProperty("返单时效")
     private LocalDateTime orderReplenishmentLeadTime;
 
+    @Schema(description = "产品长宽高")
+    @ExcelProperty("产品长宽高")
+    private String productDimensions;
+
+    @Schema(description = "产品箱规")
+    @ExcelProperty("产品箱规")
+    private String cartonSpecifications;
+
+    @Schema(description = "箱长宽高")
+    @ExcelProperty("箱长宽高")
+    private String cartonDimensions;
+
+    @Schema(description = "箱规重量")
+    @ExcelProperty("箱规重量")
+    private Double cartonWeight;
+
     @Schema(description = "发货地址")
     @ExcelProperty("发货地址")
     private String shippingAddress;
@@ -123,16 +127,16 @@ public class ErpProductRespVO {
     @ExcelProperty("退货地址")
     private String returnAddress;
 
-    @Schema(description = "物流公司")
-    @ExcelProperty("物流公司")
+    @Schema(description = "快递公司")
+    @ExcelProperty("快递公司")
     private String logisticsCompany;
 
-    @Schema(description = "不发货地")
-    @ExcelProperty("不发货地")
+    @Schema(description = "不发货区")
+    @ExcelProperty("不发货区")
     private String nonshippingArea;
 
-    @Schema(description = "加邮区域")
-    @ExcelProperty("加邮区域")
+    @Schema(description = "加邮地区")
+    @ExcelProperty("加邮地区")
     private String addonShippingArea;
 
     @Schema(description = "售后标准")
@@ -163,113 +167,73 @@ public class ErpProductRespVO {
     @ExcelProperty("团购活动最低价")
     private BigDecimal groupBuyEventMinimunPrice;
 
-    @Schema(description = "供应商")
-    @ExcelProperty("供应商")
+    @Schema(description = "采购人员")
+    @ExcelProperty("采购人员")
+    private String purchaser;
+
+    @Schema(description = "供应商名")
+    @ExcelProperty("供应商名")
     private String supplier;
 
-    @Schema(description = "代发单价")
-    @ExcelProperty("代发单价")
-    private BigDecimal dropshippingUnitPrice;
+    @Schema(description = "采购单价（单位：元）")
+    @ExcelProperty("采购单价（单位：元）")
+    private BigDecimal purchasePrice;
 
-    @Schema(description = "批发单价")
-    @ExcelProperty("批发单价")
-    private BigDecimal wholesaleUnitPrice;
+    @Schema(description = "批发单价（单位：元）")
+    @ExcelProperty("批发单价（单位：元）")
+    private BigDecimal wholesalePrice;
 
-    @Schema(description = "基础运费")
-    @ExcelProperty("基础运费")
-    private BigDecimal baseShippingFee;
+    @Schema(description = "备注信息")
+    @ExcelProperty("备注信息")
+    private String remark;
 
-    @Schema(description = "采购详情")
-    @ExcelProperty("采购详情")
-    private String purchaseDetails;
+    @Schema(description = "对外最低采购单价（单位：元）")
+    @ExcelProperty("对外最低采购单价（单位：元）")
+    private BigDecimal minPurchasePrice;
 
-    @Schema(description = "采购备注")
-    @ExcelProperty("采购备注")
-    private String purchaseNote;
+    @Schema(description = "对外最低批发单价（单位：元）")
+    @ExcelProperty("对外最低批发单价（单位：元）")
+    private BigDecimal minWholesalePrice;
 
-    @Schema(description = "销售详情")
-    @ExcelProperty("销售详情")
-    private String salesDetails;
-
-    @Schema(description = "销售备注")
-    @ExcelProperty("销售备注")
-    private String salesNote;
-
-    @Schema(description = "一级代发单价")
-    @ExcelProperty("一级代发单价")
-    private BigDecimal levelOneDropshippingPrice;
-
-    @Schema(description = "二级代发单价")
-    @ExcelProperty("二级代发单价")
-    private BigDecimal levelTwoDropshippingPrice;
-
-    @Schema(description = "一级批发单价")
-    @ExcelProperty("一级批发单价")
-    private BigDecimal levelOneWholesalePrice;
-
-    @Schema(description = "二级批发单价")
-    @ExcelProperty("二级批发单价")
-    private BigDecimal levelTwoWholesalePrice;
-
-    @Schema(description = "代发运费类型")
-    @ExcelProperty("代发运费类型")
+    @Schema(description = "运费类型（0：固定运费，1：按件计费，2：按重计费）")
+    @ExcelProperty("运费类型")
     private Integer shippingFeeType;
 
-    @Schema(description = "固定运费")
-    @ExcelProperty("固定运费")
+    @Schema(description = "固定运费（单位：元）")
+    @ExcelProperty("固定运费（单位：元）")
     private BigDecimal fixedShippingFee;
 
     @Schema(description = "首件数量")
     @ExcelProperty("首件数量")
     private Integer firstItemQuantity;
 
-    @Schema(description = "首件价格")
-    @ExcelProperty("首件价格")
+    @Schema(description = "首件价格（单位：元）")
+    @ExcelProperty("首件价格（单位：元）")
     private BigDecimal firstItemPrice;
 
     @Schema(description = "续件数量")
     @ExcelProperty("续件数量")
     private Integer additionalItemQuantity;
 
-    @Schema(description = "续件价格")
-    @ExcelProperty("续件价格")
+    @Schema(description = "续件价格（单位：元）")
+    @ExcelProperty("续件价格（单位：元）")
     private BigDecimal additionalItemPrice;
 
-    @Schema(description = "首重重量")
-    @ExcelProperty("首重重量")
+    @Schema(description = "首重重量（单位：kg）")
+    @ExcelProperty("首重重量（单位：kg）")
     private BigDecimal firstWeight;
 
-    @Schema(description = "首重价格")
-    @ExcelProperty("首重价格")
+    @Schema(description = "首重价格（单位：元）")
+    @ExcelProperty("首重价格（单位：元）")
     private BigDecimal firstWeightPrice;
 
-    @Schema(description = "首批生产日期")
-    @ExcelProperty("首批生产日期")
-    private LocalDateTime productionDate;
-
-    @Schema(description = "续重重量")
-    @ExcelProperty("续重重量")
+    @Schema(description = "续重重量（单位：kg）")
+    @ExcelProperty("续重重量（单位：kg）")
     private BigDecimal additionalWeight;
 
-    @Schema(description = "续重价格")
-    @ExcelProperty("续重价格")
+    @Schema(description = "续重价格（单位：元）")
+    @ExcelProperty("续重价格（单位：元）")
     private BigDecimal additionalWeightPrice;
-
-    @Schema(description = "保质期天数", example = "10")
-    @ExcelProperty("保质期天数")
-    private Integer expiryDay;
-
-    @Schema(description = "采购价格，单位：元", example = "10.30")
-    @ExcelProperty("采购价格，单位：元")
-    private BigDecimal purchasePrice;
-
-//    @Schema(description = "销售价格，单位：元", example = "74.32")
-//    @ExcelProperty("销售价格，单位：元")
-//    private BigDecimal salePrice;
-//
-//    @Schema(description = "最低价格，单位：元", example = "161.87")
-//    @ExcelProperty("最低价格，单位：元")
-//    private BigDecimal minPrice;
 
     @Schema(description = "创建时间", requiredMode = Schema.RequiredMode.REQUIRED)
     @ExcelProperty("创建时间")
